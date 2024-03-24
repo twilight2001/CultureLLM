625355813d0cb334b0d0c022fa4f59bf3b605cad
Scarelette <406700780@qq.com>
2024-03-23 23:29:19 -0700

    first init

--- llm_response.py
+++ llm_response.py
@@ -39,7 +39,7 @@ def getResponse(prompt, model_text, country, context, llama_model=None, llama_to
 
             url = 'https://gcrllm2-70b-chat.westus3.inference.ml.azure.com/score'
             # Replace this with the primary/secondary key or AMLToken for the endpoint
-            api_key = 'YTr1pGhu9vKuVxdi5AwhzZJwmkrByvlc'
+            api_key = ''
             if not api_key:
                 raise Exception("A key should be provided to invoke the endpoint")
 
@@ -80,15 +80,10 @@ def getResponse(prompt, model_text, country, context, llama_model=None, llama_to
         import urllib.request
         import json
         import ssl
-        key_pool = ['AIzaSyAwihsMaPr-Wiug6fdy-rkmMIQzktjgMMI', 
-                    'AIzaSyB7zS11-QBVQTP-pSqx78oqDYZ75lfJYM0', 
-                    'AIzaSyDUpVOpEevc1DN-iLEU1zy4CIbNSqB3bwI', 
-                    'AIzaSyBAUSJeOflaO20WbuMowgkKuUT7JD3l2xI', 
-                    'AIzaSyDwLHzNyEItrOh3jP6IQk4AHPlAp2aNWzQ']
+        key_pool = []
         
         key = random.choice(key_pool)
         genai.configure(api_key=key)
-        # genai.configure(api_key='AIzaSyAwihsMaPr-Wiug6fdy-rkmMIQzktjgMMI')
   
         model = genai.GenerativeModel('gemini-pro')
         # print('ok!!!')
@@ -148,7 +143,7 @@ def getResponse(prompt, model_text, country, context, llama_model=None, llama_to
         else:
             msg = [{"role": "user", "content": prompt}]
         # print('Msg: ', msg)
-        client = OpenAI(api_key="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")
+        client = OpenAI(api_key="")
 
         # print('MSG: ', msg)
 
